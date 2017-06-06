e286c38b92170a6d8f4689456f54c8d82899a647
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-06-06 10:46:35 -0700

    intial CursorAdapter info

--- java/com/example/each1/inventoryappudacity/ProductCursorAdapter.java
+++ java/com/example/each1/inventoryappudacity/ProductCursorAdapter.java
@@ -2,9 +2,15 @@ package com.example.each1.inventoryappudacity;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.each1.inventoryappudacity.data.ProductContract;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by each1 on 6/4/17.
@@ -12,16 +18,39 @@ import android.widget.CursorAdapter;
 
 public class ProductCursorAdapter extends CursorAdapter {
     public ProductCursorAdapter(Context context, Cursor c) {
-        super(context, c);
+        super(context, c, 0);
     }
 
+    // makes a new blank list item view with no data
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+
     }
+
+    //binds the product data (in the current row in the cursor) to the list-item layout
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
+        //Find the fields to populate
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price);
+
+        //Find the columns of the product attributes
+        int nameColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        int priceColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+
+        //Extract the information from those views
+        String productName = cursor.getString(nameColumnIndex);
+        int productQuantity = cursor.getInt(quantityColumnIndex);
+        int productPrice = cursor.getInt(priceColumnIndex);
+
+        //populate fields
+        nameTextView.setText(productName);
+        quantityTextView.setText(productQuantity);
+        priceTextView.setText(productPrice);
     }
 }
