cc6b4f66a40554549cf5af785c656910b7f1ab31
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-06-06 11:40:05 -0700

    initial CatalogActivity info

--- java/com/example/each1/inventoryappudacity/CatalogActivity.java
+++ java/com/example/each1/inventoryappudacity/CatalogActivity.java
@@ -1,13 +1,97 @@
 package com.example.each1.inventoryappudacity;
 
+import android.content.ContentUris;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class CatalogActivity extends AppCompatActivity {
+import com.example.each1.inventoryappudacity.data.ProductContract.ProductEntry;
+
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int PRODUCT_LOADER = 0;
+
+    ProductCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+        /*
+        * FAB button
+        //Setup FAB to open DetailActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        */
+
+        //Find the ListView to populate teh product data
+        ListView productListView = (ListView) findViewById(R.id.list);
+
+        //Find and set empty view on the ListView, so it shows when list has 0 values
+        View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);
+
+        //Setup an adapter to create a list item for each row of product data in the Cursor
+        //there is no data (until the loader finishes) so pass null in for Cursor
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        //Setup item click listener
+        productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Create the new Intent to go to DetailActivity
+                Intent intent = new Intent (CatalogActivity.this, DetailActivity.class);
+
+                //Form the content Uri, append id of item clicked
+                Uri currentProductUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+
+                intent.setData(currentProductUri);
+                
+                startActivity(intent);
+            }
+        });
+
+        //Start the loader
+        getSupportLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        //Define a projection of columns to bring to the list
+        String [] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_PRICE};
+        //Loader will execute ContentProvider's query method on background thread
+        return new CursorLoader(this,
+                ProductEntry.CONTENT_URI,
+                projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
