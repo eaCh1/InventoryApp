31c892e27951cb3309da1126311b7e543f93a5c8
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-06-06 10:48:24 -0700

    changed price from REAL/FLOAT to INTEGER

--- java/com/example/each1/inventoryappudacity/data/ProductDbHelper.java
+++ java/com/example/each1/inventoryappudacity/data/ProductDbHelper.java
@@ -30,7 +30,7 @@ public class ProductDbHelper extends SQLiteOpenHelper {
         String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE" + ProductEntry.TABLE_NAME + " ("
                 + ProductEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
-                + ProductEntry.COLUMN_PRODUCT_PRICE + " REAL NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_PRICE + "INTEGER NOT NULL, "
                 + ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NO NULL DEFAULT 0, "
                 + ProductEntry.COLUMN_PRODUCT_SUPPLIER + " TEXT NOT NULL, "
                 + ProductEntry.COLUMN_PRODUCT_PICTURE + " TEXT);";
