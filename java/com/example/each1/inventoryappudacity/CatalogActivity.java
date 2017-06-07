c8f9efc426c1d5de8ca3e9df605c31085750dff7
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-06-07 09:01:50 -0700

    linked Catalog FAB to DetailActivity

--- java/com/example/each1/inventoryappudacity/CatalogActivity.java
+++ java/com/example/each1/inventoryappudacity/CatalogActivity.java
@@ -1,10 +1,9 @@
 package com.example.each1.inventoryappudacity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class CatalogActivity extends AppCompatActivity {
@@ -20,8 +19,8 @@ public class CatalogActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
             }
         });
     }
--- java/com/example/each1/inventoryappudacity/DetailActivity.java
+++ java/com/example/each1/inventoryappudacity/DetailActivity.java
@@ -1,8 +1,19 @@
 package com.example.each1.inventoryappudacity;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
 /**
  * Created by each1 on 6/4/17.
  */
 
-public class DetailActivity {
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
 }
