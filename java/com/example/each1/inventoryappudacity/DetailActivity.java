a3d4f33144a87748b435d88ae6a115482b44d9c3
Eric Andrew Chunn <ericandrewchunn@gmail.com>
2017-06-10 20:35:32 -0700

    detail activity working, adding more loader details

--- java/com/example/each1/inventoryappudacity/DetailActivity.java
+++ java/com/example/each1/inventoryappudacity/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.each1.inventoryappudacity;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -7,11 +8,14 @@ import android.media.Image;
 import android.os.Bundle;
 import android.net.Uri;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.database.Cursor;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
@@ -45,6 +49,9 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
 
     //Edit text field to enter the product's quantity
     private EditText mQuantityEditText;
+
+    //Edit text field to enter the supplier's email
+    private EditText mSupplierEditText;
 
     // Image view to hold the product's image
     private ImageView mProductImageView;
@@ -162,28 +169,6 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        //if the pet hasn't changed, continue with the handling back button press
-        if (!mProductHasChanged) {
-            super.onBackPressed();
-            return;
-        }
-
-        //otherwise if there are unsaved changes, setup a dialog to warn the user
-        //create a click listener to handle the user confirming that changes should be discarded
-        DialogInterface.OnClickListener discardButtonClickListener =
-                new DialogInterface.OnClickListener() {
-
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //User clicked the "discard" button, close the current activity
-                        finish();
-                    }
-                };
-
-        //showUnsavedChangesDialog(discardButtonClickListener);
-    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -201,7 +186,36 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        //Bail early if the cursor is null
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+
+
+        if (cursor.moveToFirst()) {
+            //Find the names of the columns
+            int nameColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+            int supplierColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER);
+            int pictureColumnIndex = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+
+
+            //Extract the values
+            String name = cursor.getString(nameColumnIndex);
+            int quantity = cursor.getInt(quantityColumnIndex);
+            int price = cursor.getInt(priceColumnIndex);
+            String supplier = cursor.getString(supplierColumnIndex);
+            String picture = cursor.getString(pictureColumnIndex);
+
+            //Update the views on the screen
+            mPriceEditText.setText(name);
+            mQuantityEditText.setText(quantity);
+            mPriceEditText.setText(price);
+            mSupplierEditText.setText(supplier);
+
+        }
 
 
     }
@@ -213,5 +227,74 @@ public class DetailActivity extends AppCompatActivity implements LoaderManager.L
         mPriceEditText.setText("");
         mQuantityEditText.setText("");
 
+    }
+
+    private void showUnsavedChangesDialog(
+            DialogInterface.OnClickListener discardButtonClickListener) {
+        //Create an AlertDialog.Builder and set the message, and click listeners
+        //for the positive and negative buttons on the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unsaved_changes_dialog_msg);
+        builder.setPositiveButton(R.string.discard, discardButtonClickListener);
+        builder.setNegativeButton(R.string.keep_editing, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //User clicked the "Keep editing" button, so dimiss the dialog
+                // and continue editing the pet
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Delete" button, so delete the pet.
+                deletePet();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                // and continue editing the pet.
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deletePet() {
+        if (mCurrentProductUri != null) {
+            //Call the Content Resolver to delete the pet at the given content URI
+            //Pass in null for the selection and selection args because the mCurrentPetUri
+            //content URI already identifies the pet that we want
+            int rowsDeleted = getContentResolver().delete(mCurrentProductUri, null, null);
+
+            //show a toast message depending on whether or not the delete was succesful
+            if (rowsDeleted == 0) {
+                Toast.makeText(this, getString(R.string.detail_delete_product_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                //Otherwise, the delete was successful and we can display a toast
+                Toast.makeText(this, getString(R.string.detail_insert_product_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        //Close the activity
+        finish();
     }
 }
